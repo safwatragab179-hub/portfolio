d20780856d5570564095c63fcabd0ca127f5cb9f
safwatragab179-hub <safwatragab179@gmail.com>
2026-09-24 17:54:41 +0300

    Add cybersecurity project

---
+++ script.js
@@ -0,0 +1,1 @@
+const menu=document.getElementById("menu"),links=document.getElementById("links");menu.addEventListener("click",()=>links.classList.toggle("open"));document.querySelectorAll("#links a").forEach(a=>a.addEventListener("click",()=>links.classList.remove("open")));
